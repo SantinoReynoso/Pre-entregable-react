92f41bacc1e6c9a7261f3b5cc9a0e94bd2237aa6
Santino Reynoso <santinoreynoso5@gmail.com>
2024-05-26 17:29:45 -0300

    entrega con sin las variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyCYL39isIcywhvZzCBx9IVTCuIDn367NmM",
+  authDomain: "fir-san-69b0a.firebaseapp.com",
+  projectId: "fir-san-69b0a",
+  storageBucket: "fir-san-69b0a.appspot.com",
+  messagingSenderId: "128880650083",
+  appId: "1:128880650083:web:3d3378c972fe20a4d09556"
 };
 
 const app = initializeApp(firebaseConfig);
