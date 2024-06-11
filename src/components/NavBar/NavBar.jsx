5ece6d54f3721dac2617d682c9dd42fe0c11d77e
Santino Reynoso <santinoreynoso5@gmail.com>
2024-06-11 17:13:11 -0300

    listo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Container, Flex, Image, Text } from '@chakra-ui/react';
 import lalentejaImage from "../../assets/lalenteja.jpg";
 import CartWidget from '../CardWidget/CartWidget';
@@ -15,19 +15,19 @@ const NavBar = () => {
         {/* Imagen de La Lenteja */}
         <Image src={lalentejaImage} alt="La Lenteja" boxSize="150px" mr="3" borderRadius="35px" boxShadow="2xl"/>
         <Flex flex="1" />
-        <Link to="/" style={{ textDecoration: 'none' }}>
+        <NavLink to="/" style={{ textDecoration: 'none' }} activeclassname="active-link">
           <Text fontSize="lg" color="white" mr="4" cursor="pointer">Inicio</Text>
-        </Link>
-        <Link to="/productos" style={{ textDecoration: 'none' }}>
+        </NavLink>
+        <NavLink to="/productos" style={{ textDecoration: 'none' }} activeclassname="active-link">
           <Text fontSize="lg" color="white" mr="4" cursor="pointer">Productos</Text>
-        </Link>
+        </NavLink>
         <Text fontSize="lg" color="white" mr="4" cursor="not-allowed">Contacto</Text>
         <Text fontSize="lg" color="white" cursor="not-allowed">Iniciar Sesión</Text>
       </Flex>
       <Flex justify="flex-end">
-        <Link to="/cartpage" style={{ textDecoration: 'none' }}>
+        <NavLink to="/cartpage" style={{ textDecoration: 'none' }} activeclassname="active-link">
           <CartWidget itemCount={cartItemCount} />
-        </Link>
+        </NavLink>
       </Flex>
     </Container>
   );
